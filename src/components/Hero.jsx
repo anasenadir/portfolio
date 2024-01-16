f2ff558d67e6bde1bfe7f0872aaf29cc8ad7ba21
anasen <anasenadir2001@gmail.com>
2024-01-16 12:48:19 -0800

    make the arrow-down hinter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
             Hi, I'm
             <span className='text-[#915eff]'> Anas</span>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              As a full-stack developer with 
+              I'm a full-stack developer with 
               <br className='sm:block hidden'/>
                over 2 years of experience.
             </p>
@@ -29,6 +29,24 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 
+          border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y:[0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: Infinity
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
