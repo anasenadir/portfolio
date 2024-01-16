36f9371046eb8b1e2098e441adf040f7e9fa6339
anasen <anasenadir2001@gmail.com>
2024-01-16 11:59:18 -0800

    fix some errors on The  Computer Component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -38,16 +38,15 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
         <Computers/>
+        <Preload all /> 
       </Suspense>
-
-      <Preload all />
     </Canvas>
   )
 }
